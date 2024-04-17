cfac513d7395abccfce7e70e3e84174f2ce83419
Ankit-69k <ankitkumarray19@gmail.com>
2024-04-17 08:22:43 +0530

    added shadcn lib components

---
+++ client/src/components/headCard.tsx
@@ -0,0 +1,54 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+
+function HeadCard() {
+  return (
+    <div className="flex items-center justify-between space-y-2">
+      <div className="text-left">
+        <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+        <p className="text-muted-foreground">
+          Here&apos;s a list of your tasks for this month!
+        </p>
+      </div>
+      <div className="flex items-center space-x-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+              <Avatar className="h-9 w-9">
+                <AvatarImage src="/avatars/03.png" alt="@shadcn" />
+                <AvatarFallback>AK</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">Ankit</p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  ak_69@gmail.com
+                </p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Log out</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
+
+export default HeadCard;
--- server/main.go
+++ server/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"toDO_list/server/middleware"
+	"toDO_list/server/routes"
+	"toDO_list/server/models"
+)
+
 func main(){
-	
+	r := routes.Router()
+	fmt.Println("Starting server on the port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
